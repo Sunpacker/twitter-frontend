8ed1e9f8248c1ae83fc7b854c48188961b7d37f3
Sunpacker <sunpacker@yandex.ru>
2020-11-09 12:03:19 +0400

    tweets/fetch data from real server

--- src/api/tweets.ts
+++ src/api/tweets.ts
@@ -1,14 +1,20 @@
 import axios from 'axios'
-import { ITweet, ITweets } from '../store/ducks/tweets/state'
+import { ITweet } from '../store/ducks/tweets/state'
+import { API_URL } from './vars'
 
+
+interface IResponse<T> {
+	status: string;
+	data: T
+}
 
 export const TweetsApi = {
-	async fetchTweets(): Promise<ITweets['items']> {
-		const { data } = await axios.get('https://trycode.pw/c/2OBQ1.json')
-		return data
+	async fetchTweets(): Promise<IResponse<ITweet[]>> {
+		const response = await axios.get(`${API_URL}/tweets`)
+		return response.data.data
 	},
-	async addTweet(payload: ITweet): Promise<ITweet> {
-		const { data } = await axios.post('https://trycode.pw/c/2OBQ1.json', payload)
-		return data
+	async addTweet(payload: ITweet): Promise<IResponse<ITweet>> {
+		const response = await axios.post(`${API_URL}/tweets`, payload)
+		return response.data.data
 	},
 }
